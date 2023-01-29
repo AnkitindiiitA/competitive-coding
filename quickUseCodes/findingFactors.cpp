5e847752af9ee0c8665d253de24c1070d5651d51
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2023-01-30 03:05:56 +0530

    Update findingFactors.cpp

--- quickUseCodes/findingFactors.cpp
+++ quickUseCodes/findingFactors.cpp
@@ -24,3 +24,17 @@ if(n>1){
     ans.push_back({1,n});
     n=1;
 }
+
+
+//by storing count int map
+ll n; cin>>n;
+        map<int,int>mp;
+        for(int i=2 ; i*i<=n; ++i){
+            while(n%i==0){
+                mp[i]++;
+                n/=i;
+            }
+        }
+        if(n>1) mp[n]++;
+
+//iterate n^2 times to access layerwise elements and decrease map count of each element by 1;

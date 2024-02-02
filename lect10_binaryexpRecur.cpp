a953252d592f572d6654f0faa94fe013db118b19
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2024-02-03 01:06:49 +0530

    Update lect10_binaryexpRecur.cpp

--- lect10_binaryexpRecur.cpp
+++ lect10_binaryexpRecur.cpp
@@ -29,7 +29,7 @@ int biExpIter(int a, int b){
     int ans=1;
     while(b){
         if(b&1){
-            ans=(ans*a)%M;
+            ans=(ans*1LL*a)%M;
         }
         a=(a*1LL*a)%M;
         b>>=1;

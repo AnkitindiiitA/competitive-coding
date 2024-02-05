f544472d08ff4d3c31148e57175da55b2b8acac8
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2024-02-05 13:55:30 +0530

    Update findAllUniquePrimeFcatorsOfANumber.cpp

--- findAllUniquePrimeFcatorsOfANumber.cpp
+++ findAllUniquePrimeFcatorsOfANumber.cpp
@@ -29,3 +29,46 @@ int main(){
   cout<<"\n";
   return 0;
 }
+
+
+//using seive for O(logn)
+
+#include <bits/stdc++. h> 
+using namespace std; 
+int N = 1000000; 
+int spf [N + 1]; 
+void createSieve() {
+     for(int i = 1;i <= N;i++) { 
+          spf[i] = i;
+     }
+     for(int i = 2;i*i <= N; i++) { 
+          if(spf[i] == i) {
+               for(int j = i*i; j <= n; j+=i) { 
+                         if(spf[j] == j) { 
+                              spf[j] = i;
+                         }
+               }
+          }
+     }
+}
+
+int main(){
+     
+     createSieve();
+     int t;
+     cin>>t;
+     while(t--){
+          int n;
+          cin>>n;
+          
+          while(n!=1){
+               cout<<spf[n]<<' ';
+               n/=spf[n];
+          }
+          cout<<"\n";
+          
+     }
+     
+     
+     return 0;
+}
